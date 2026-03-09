a27e6b2209c4326756ab3df73c33c7bb3ff7fa84
Seif Emam <seifmoataz27249@gmail.com>
2026-03-10 00:57:24 +0200

    Refactor user profile update process
    - Removed dependency on `UpdateUserProfileOrchestrator` in `AuthController.cs` and utilized `IMediator` for handling profile updates.
    - Improved readability and modernized method signatures in `UpdateUserProfileHandler.cs`.
    - Updated `UpdateUserProfileOrchestrator.cs` to implement `IRequestHandler` and refactored image upload handling.
    - Introduced `UpdateUserProfileOrchestratorRequest.cs` to encapsulate parameters for user profile updates.
    - Overall enhancements to code readability, maintainability, and adherence to modern C# practices.

---
+++ AuthenticationService/Features/Auth/UpdateUserProfile/UpdateUserProfileOrchestratorRequest.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace AuthenticationService.Features.Auth.UpdateUserProfile
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Http;
+
+    namespace AuthenticationService.Features.Auth.UpdateUserProfile
+    {
+        public record UpdateUserProfileOrchestratorRequest(
+            Guid UserId,
+            string? FirstName,
+            string? LastName,
+            string? PhoneNumber,
+            string Goal,
+            string activtyLevel,
+            double Weight,
+            double Height,
+            IFormFile? ProfileImage
+        ) : IRequest<UpdateUserProfileResponse>;
+    }
+}
